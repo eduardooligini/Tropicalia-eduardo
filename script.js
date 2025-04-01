a6ce161b8f1ac5dfb25ef75535e7dbfe1af46d73
eduardooligini <145785071+eduardooligini@users.noreply.github.com>
2025-04-01 08:25:53 -0300

    01/04

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document. addEventListener('DOMContentLoaded' , function(){
+document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
 
     let tamanhoAtualFonte = 1;
